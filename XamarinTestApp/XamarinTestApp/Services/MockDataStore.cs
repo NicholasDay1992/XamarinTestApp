713af68929831df0685bb63e4e85d584cdf42e10
NicholasDay1992 <48986277+nickday1992@users.noreply.github.com>
2021-07-06 19:11:52 +0100

    Modified generic items to resemble Apps

--- XamarinTestApp/XamarinTestApp/Services/MockDataStore.cs
+++ XamarinTestApp/XamarinTestApp/Services/MockDataStore.cs
@@ -14,12 +14,12 @@ namespace XamarinTestApp.Services
         {
             items = new List<Item>()
             {
-                new Item { Id = Guid.NewGuid().ToString(), Text = "First item", Description="This is an item description." },
-                new Item { Id = Guid.NewGuid().ToString(), Text = "Second item", Description="This is an item description." },
-                new Item { Id = Guid.NewGuid().ToString(), Text = "Third item", Description="This is an item description." },
-                new Item { Id = Guid.NewGuid().ToString(), Text = "Fourth item", Description="This is an item description." },
-                new Item { Id = Guid.NewGuid().ToString(), Text = "Fifth item", Description="This is an item description." },
-                new Item { Id = Guid.NewGuid().ToString(), Text = "Sixth item", Description="This is an item description." }
+                new Item { Id = Guid.NewGuid().ToString(), Text = "App01", Description="Page for App01" },
+                new Item { Id = Guid.NewGuid().ToString(), Text = "App02", Description="Page for App02" },
+                new Item { Id = Guid.NewGuid().ToString(), Text = "App03", Description="Page for App03" },
+                new Item { Id = Guid.NewGuid().ToString(), Text = "App04", Description="Page for App04" },
+                new Item { Id = Guid.NewGuid().ToString(), Text = "App05", Description="Page for App05" },
+                new Item { Id = Guid.NewGuid().ToString(), Text = "App06", Description="Page for App06" }
             };
         }
 
