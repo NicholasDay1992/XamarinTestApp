314f4b8cb03b32bd6df1b3dc0b21e357434b7e79
NicholasDay1992 <48986277+nickday1992@users.noreply.github.com>
2021-07-06 18:47:33 +0100

    Changed link from MS to BBC

--- XamarinTestApp/XamarinTestApp/ViewModels/AboutViewModel.cs
+++ XamarinTestApp/XamarinTestApp/ViewModels/AboutViewModel.cs
@@ -10,7 +10,7 @@ namespace XamarinTestApp.ViewModels
         public AboutViewModel()
         {
             Title = "About";
-            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
+            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://www.bbc.co.uk"));
         }
 
         public ICommand OpenWebCommand { get; }
