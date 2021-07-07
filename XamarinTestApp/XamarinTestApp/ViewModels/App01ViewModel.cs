55064db441d47d3df1a2de434dec72db8bb7496d
NicholasDay1992 <48986277+nickday1992@users.noreply.github.com>
2021-07-07 08:54:19 +0100

    Attempting to move App01 logic to dedicated page

---
+++ XamarinTestApp/XamarinTestApp/ViewModels/App01ViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace XamarinTestApp.ViewModels
+{
+    public class App01ViewModel : BaseViewModel
+    {
+        public App01ViewModel()
+        {
+            Title = "App01";
+            /*
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Welcome to Xamarin.Forms!" }
+                }
+            };
+            */
+        }
+    }
+}
