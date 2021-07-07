95e6d5eb6d9b329d9174af68b8684b67662f66a9
NicholasDay1992 <48986277+nickday1992@users.noreply.github.com>
2021-07-07 08:40:27 +0100

    Attempting to link App01 page into tab bar

--- XamarinTestApp/XamarinTestApp/AppShell.xaml.cs
+++ XamarinTestApp/XamarinTestApp/AppShell.xaml.cs
@@ -13,6 +13,7 @@ namespace XamarinTestApp
             InitializeComponent();
             Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
             Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+            Routing.RegisterRoute(nameof(App01), typeof(App01));
         }
 
     }
