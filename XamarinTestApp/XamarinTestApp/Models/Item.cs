716de0029740fc2e0079b49fc58c161fffab9528
NicholasDay1992 <48986277+nickday1992@users.noreply.github.com>
2021-07-26 15:58:40 +0100

    Adding variables for App01

--- XamarinTestApp/XamarinTestApp/Models/Item.cs
+++ XamarinTestApp/XamarinTestApp/Models/Item.cs
@@ -7,5 +7,10 @@ namespace XamarinTestApp.Models
         public string Id { get; set; }
         public string Text { get; set; }
         public string Description { get; set; }
+
+        public double fromDistance { get; set; }
+        public double toDistance { get; set; }
+        public string fromUnit { get; set; }
+        public string toUnit { get; set; }
     }
 }
